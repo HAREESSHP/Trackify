93af454398b9bbf505be5bf8502ec3db6bb7a46e
Hareesh <bagayithiharish@gmail.com>
2025-05-30 10:00:44 +0530

    ipadress

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors({
   origin: [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
-    'https://trackify-gv4i.onrender.com/' // <-- add your deployed frontend URL here
+    'https://trackify-gv4i.onrender.com' // <-- add your deployed frontend URL here
   ],
   credentials: true
 }));
