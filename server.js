5a044b471ad92332b3e9c187c64cfd8bf7745381
Hareesh <bagayithiharish@gmail.com>
2025-05-30 09:48:01 +0530

    updates

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+  origin: [
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+    'https://trackify-gv4i.onrender.com/' // <-- add your deployed frontend URL here
+  ],
   credentials: true
 }));
 
